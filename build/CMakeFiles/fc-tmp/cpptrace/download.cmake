8334b7f7c4935b9d500ffdcf24920af88d914ac1
unknown <khubofficial00@gmail.com>
2025-08-31 07:49:10 +0300

    Remove accidentally committed build folder

--- build/CMakeFiles/fc-tmp/cpptrace/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing download step for cpptrace")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/Predator/Documents/contribute/Nuvio/build/CMakeFiles/fc-tmp/cpptrace/cpptrace-gitclone.cmake")
-
-endblock()
